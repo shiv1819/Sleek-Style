c2dde2edc09c554b1cd8409677ee6827d06c2c86
shiv1819 <shivampanchal11q@gmail.com>
2024-07-26 19:17:25 +0530

    Files Uploaded

---
+++ assets/js/about.js
@@ -0,0 +1,18 @@
+
+let mybutton = document.getElementById("top-btn");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
---
+++ assets/js/shop.js
@@ -0,0 +1,293 @@
+function p1() {
+    document.getElementById("p1").style.display = "block";
+} 
+function p2() {
+
+    document.getElementById("p2").style.display = "block";
+} function p3() {
+    document.getElementById("p3").style.display = "block";
+} function p4() {
+    document.getElementById("p4").style.display = "block";
+} function p5() {
+    document.getElementById("p5").style.display = "block";
+} function p6() {
+    document.getElementById("p6").style.display = "block";
+} function p7() {
+    document.getElementById("p7").style.display = "block";
+} function p8() {
+    document.getElementById("p8").style.display = "block";
+} function p9() {
+    document.getElementById("p9").style.display = "block";
+} function p10() {
+    document.getElementById("p10").style.display = "block";
+} function p11() {
+    document.getElementById("p11").style.display = "block";
+} function p12() {
+    document.getElementById("p12").style.display = "block";
+} function p13() {
+    document.getElementById("p13").style.display = "block";
+} function p14() {
+    document.getElementById("p14").style.display = "block";
+} function p15() {
+    document.getElementById("p15").style.display = "block";
+} function p16() {
+    document.getElementById("p16").style.display = "block";
+}
+function cancel() {
+
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("p2").style.display = "none";
+    document.getElementById("p3").style.display = "none";
+    document.getElementById("p4").style.display = "none";
+    document.getElementById("p5").style.display = "none";
+    document.getElementById("p6").style.display = "none";
+    document.getElementById("p7").style.display = "none";
+    document.getElementById("p8").style.display = "none";
+    document.getElementById("p9").style.display = "none";
+    document.getElementById("p10").style.display = "none";
+    document.getElementById("p11").style.display = "none";
+    document.getElementById("p12").style.display = "none";
+    document.getElementById("p13").style.display = "none";
+    document.getElementById("p14").style.display = "none";
+    document.getElementById("p15").style.display = "none"
+    document.getElementById("p16").style.display = "none";
+
+}
+function plain() {
+    document.getElementById("plain").style.color = "red";
+    document.getElementById("all").style.color = "#666666";
+    document.getElementById("oversized").style.color = "#666666";
+    document.getElementById("printed").style.color = "#666666";
+    document.getElementById("polo").style.color = "#666666";
+    document.getElementById("card-1").style.display = "none";
+    document.getElementById("card-2").style.display = "none";
+    document.getElementById("card-3").style.display = "none";
+    document.getElementById("card-4").style.display = "none";
+    document.getElementById("card-5").style.display = "none";
+    document.getElementById("card-6").style.display = "none";
+    document.getElementById("card-7").style.display = "none";
+    document.getElementById("card-8").style.display = "none";
+    document.getElementById("card-9").style.display = "block";
+    document.getElementById("card-10").style.display = "block";
+    document.getElementById("card-11").style.display = "block";
+    document.getElementById("card-12").style.display = "block";
+    document.getElementById("card-13").style.display = "none";
+    document.getElementById("card-14").style.display = "none";
+    document.getElementById("card-15").style.display = "none";
+    document.getElementById("card-16").style.display = "none";
+    document.getElementById("card-1").style.transition = "1.5s";
+    document.getElementById("card-2").style.transition = "1.5s";
+    document.getElementById("card-3").style.transition = "1.5s";
+    document.getElementById("card-4").style.transition = "1.5s";
+    document.getElementById("card-5").style.transition = "1.5s";
+    document.getElementById("card-6").style.transition = "1.5s";
+    document.getElementById("card-7").style.transition = "1.5s";
+    document.getElementById("card-8").style.transition = "1.5s";
+    document.getElementById("card-9").style.transition = "1.5s";
+    document.getElementById("card-10").style.transition = "1.5s";
+    document.getElementById("card-11").style.transition = "1.5s";
+    document.getElementById("card-12").style.transition = "1.5s";
+    document.getElementById("card-13").style.transition = "1.5s";
+    document.getElementById("card-14").style.transition = "1.5s";
+    document.getElementById("card-15").style.transition = "1.5s";
+    document.getElementById("card-16").style.transition = "1.5s";
+}
+function All() {
+    document.getElementById("all").style.color = "red";
+    document.getElementById("plain").style.color = "#666666";
+    document.getElementById("oversized").style.color = "#666666";
+    document.getElementById("printed").style.color = "#666666";
+    document.getElementById("polo").style.color = "#666666";
+    document.getElementById("card-1").style.display = "block";
+    document.getElementById("card-2").style.display = "block";
+    document.getElementById("card-3").style.display = "block";
+    document.getElementById("card-4").style.display = "block";
+    document.getElementById("card-5").style.display = "block";
+    document.getElementById("card-6").style.display = "block";
+    document.getElementById("card-7").style.display = "block";
+    document.getElementById("card-8").style.display = "block";
+    document.getElementById("card-9").style.display = "block";
+    document.getElementById("card-10").style.display = "block";
+    document.getElementById("card-11").style.display = "block";
+    document.getElementById("card-12").style.display = "block";
+    document.getElementById("card-13").style.display = "block";
+    document.getElementById("card-14").style.display = "block";
+    document.getElementById("card-15").style.display = "block";
+    document.getElementById("card-16").style.display = "block";
+    document.getElementById("card-1").style.transition = "1.5s";
+    document.getElementById("card-2").style.transition = "1.5s";
+    document.getElementById("card-3").style.transition = "1.5s";
+    document.getElementById("card-4").style.transition = "1.5s";
+    document.getElementById("card-5").style.transition = "1.5s";
+    document.getElementById("card-6").style.transition = "1.5s";
+    document.getElementById("card-7").style.transition = "1.5s";
+    document.getElementById("card-8").style.transition = "1.5s";
+    document.getElementById("card-9").style.transition = "1.5s";
+    document.getElementById("card-10").style.transition = "1.5s";
+    document.getElementById("card-11").style.transition = "1.5s";
+    document.getElementById("card-12").style.transition = "1.5s";
+    document.getElementById("card-13").style.transition = "1.5s";
+    document.getElementById("card-14").style.transition = "1.5s";
+    document.getElementById("card-15").style.transition = "1.5s";
+    document.getElementById("card-16").style.transition = "1.5s";
+}
+function printed() {
+    document.getElementById("printed").style.color = "red";
+    document.getElementById("all").style.color = "#666666";
+    document.getElementById("plain").style.color = "#666666";
+    document.getElementById("oversized").style.color = "#666666";
+    document.getElementById("polo").style.color = "#666666";
+    document.getElementById("card-1").style.display = "block";
+    document.getElementById("card-2").style.display = "block";
+    document.getElementById("card-3").style.display = "block";
+    document.getElementById("card-4").style.display = "none";
+    document.getElementById("card-5").style.display = "block";
+    document.getElementById("card-6").style.display = "none";
+    document.getElementById("card-7").style.display = "block";
+    document.getElementById("card-8").style.display = "block";
+    document.getElementById("card-9").style.display = "none";
+    document.getElementById("card-10").style.display = "none";
+    document.getElementById("card-11").style.display = "none";
+    document.getElementById("card-12").style.display = "none";
+    document.getElementById("card-13").style.display = "none";
+    document.getElementById("card-14").style.display = "block";
+    document.getElementById("card-15").style.display = "block";
+    document.getElementById("card-16").style.display = "block";
+    document.getElementById("card-1").style.transition = "1.5s";
+    document.getElementById("card-2").style.transition = "1.5s";
+    document.getElementById("card-3").style.transition = "1.5s";
+    document.getElementById("card-4").style.transition = "1.5s";
+    document.getElementById("card-5").style.transition = "1.5s";
+    document.getElementById("card-6").style.transition = "1.5s";
+    document.getElementById("card-7").style.transition = "1.5s";
+    document.getElementById("card-8").style.transition = "1.5s";
+    document.getElementById("card-9").style.transition = "1.5s";
+    document.getElementById("card-10").style.transition = "1.5s";
+    document.getElementById("card-11").style.transition = "1.5s";
+    document.getElementById("card-12").style.transition = "1.5s";
+    document.getElementById("card-13").style.transition = "1.5s";
+    document.getElementById("card-14").style.transition = "1.5s";
+    document.getElementById("card-15").style.transition = "1.5s";
+    document.getElementById("card-16").style.transition = "1.5s";
+}
+function oversized() {
+    document.getElementById("oversized").style.color = "red";
+    document.getElementById("printed").style.color = "#666666";
+    document.getElementById("all").style.color = "#666666";
+    document.getElementById("plain").style.color = "#666666";
+    document.getElementById("polo").style.color = "#666666";
+    document.getElementById("card-1").style.display = "none";
+    document.getElementById("card-2").style.display = "block";
+    document.getElementById("card-3").style.display = "block";
+    document.getElementById("card-4").style.display = "none";
+    document.getElementById("card-5").style.display = "block";
+    document.getElementById("card-6").style.display = "none";
+    document.getElementById("card-7").style.display = "block";
+    document.getElementById("card-8").style.display = "block";
+    document.getElementById("card-9").style.display = "none";
+    document.getElementById("card-10").style.display = "block";
+    document.getElementById("card-11").style.display = "none";
+    document.getElementById("card-12").style.display = "block";
+    document.getElementById("card-13").style.display = "none";
+    document.getElementById("card-14").style.display = "none";
+    document.getElementById("card-15").style.display = "block";
+    document.getElementById("card-16").style.display = "block";
+    document.getElementById("card-1").style.transition = "1.5s";
+    document.getElementById("card-2").style.transition = "1.5s";
+    document.getElementById("card-3").style.transition = "1.5s";
+    document.getElementById("card-4").style.transition = "1.5s";
+    document.getElementById("card-5").style.transition = "1.5s";
+    document.getElementById("card-6").style.transition = "1.5s";
+    document.getElementById("card-7").style.transition = "1.5s";
+    document.getElementById("card-8").style.transition = "1.5s";
+    document.getElementById("card-9").style.transition = "1.5s";
+    document.getElementById("card-10").style.transition = "1.5s";
+    document.getElementById("card-11").style.transition = "1.5s";
+    document.getElementById("card-12").style.transition = "1.5s";
+    document.getElementById("card-13").style.transition = "1.5s";
+    document.getElementById("card-14").style.transition = "1.5s";
+    document.getElementById("card-15").style.transition = "1.5s";
+    document.getElementById("card-16").style.transition = "1.5s";
+}
+
+function polo() {
+    document.getElementById("polo").style.color = "red";
+    document.getElementById("oversized").style.color = "#666666";
+    document.getElementById("printed").style.color = "#666666";
+    document.getElementById("all").style.color = "#666666";
+    document.getElementById("plain").style.color = "#666666";
+    document.getElementById("card-1").style.display = "none";
+    document.getElementById("card-2").style.display = "none";
+    document.getElementById("card-3").style.display = "none";
+    document.getElementById("card-4").style.display = "block";
+    document.getElementById("card-5").style.display = "none";
+    document.getElementById("card-6").style.display = "block";
+    document.getElementById("card-7").style.display = "none";
+    document.getElementById("card-8").style.display = "none";
+    document.getElementById("card-9").style.display = "none";
+    document.getElementById("card-10").style.display = "none";
+    document.getElementById("card-11").style.display = "none";
+    document.getElementById("card-12").style.display = "none";
+    document.getElementById("card-13").style.display = "block";
+    document.getElementById("card-14").style.display = "none";
+    document.getElementById("card-15").style.display = "none";
+    document.getElementById("card-16").style.display = "none";
+    document.getElementById("card-1").style.transition = "1.5s";
+    document.getElementById("card-2").style.transition = "1.5s";
+    document.getElementById("card-3").style.transition = "1.5s";
+    document.getElementById("card-4").style.transition = "1.5s";
+    document.getElementById("card-5").style.transition = "1.5s";
+    document.getElementById("card-6").style.transition = "1.5s";
+    document.getElementById("card-7").style.transition = "1.5s";
+    document.getElementById("card-8").style.transition = "1.5s";
+    document.getElementById("card-9").style.transition = "1.5s";
+    document.getElementById("card-10").style.transition = "1.5s";
+    document.getElementById("card-11").style.transition = "1.5s";
+    document.getElementById("card-12").style.transition = "1.5s";
+    document.getElementById("card-13").style.transition = "1.5s";
+    document.getElementById("card-14").style.transition = "1.5s";
+    document.getElementById("card-15").style.transition = "1.5s";
+    document.getElementById("card-16").style.transition = "1.5s";
+}
+let mybutton = document.getElementById("top-btn");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+      
+      
+    const cartButtons = document.querySelectorAll('.cart-button');
+
+     cartButtons.forEach(button => {
+
+              button.addEventListener('click',cartClick);
+
+     });
+
+     function cartClick(){
+      let button =this;
+      button.classList.add('clicked');
+     }
+    
+    
+    
+});
+var c=0;
+function cart(){
+    c++;
+    document.getElementById("cart").innerHTML=c;
+}
